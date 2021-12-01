9cb4f3cf28d667c3742a05814ac0f9163b62d354
MischaGe <gemsa.michael@gmail.com>
2021-12-01 21:52:18 +0100

    firstcommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("losgehts");
+        System.out.println("losgehtsssss");
 
         Author auth = new Author("jkrowling", 50);
         Book buch = new Book(1, "harryprozzer", 1000, auth);
